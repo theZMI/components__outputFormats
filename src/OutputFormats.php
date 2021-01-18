1e9432f08af3c24e7852f2fbd0962973ef6e0f58
zmi zaytsev <zmigpost@gmail.com>
2021-01-18 19:45:48 +0200

    Change code format file

--- src/OutputFormats.php
+++ src/OutputFormats.php
@@ -95,6 +95,7 @@ class OutputFormats
         $part_3 = substr($phone, 5, 3);
         $part_4 = substr($phone, 8, 2);
         $part_5 = substr($phone, 10, 2);
+
         return "{$part_1}({$part_2}){$part_3}-{$part_4}-{$part_5}";
     }
 }
